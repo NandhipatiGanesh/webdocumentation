91d6aa4f24d0838d9095d64533e2a92c1507c92b
Ganesh <205735160+luxHospitlas@users.noreply.github.com>
2025-05-30 21:59:25 +0530

    h

--- src/components/snippetscomp/Reactdesigns.tsx
+++ src/components/snippetscomp/Reactdesigns.tsx
@@ -38,7 +38,7 @@ export default function ReactComponents() {
       {loading ? (
         <SpinnerDemo />
       ) : (
-        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1 mt-8">
+        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mt-8">
           {postdata.map((item, index) => (
             <a
               key={index}
