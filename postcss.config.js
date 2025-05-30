fa9878d42a2ffed4c562a671d50e0f2bf683d48d
Ganesh <205735160+luxHospitlas@users.noreply.github.com>
2025-05-30 22:27:32 +0530

    purge added

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
+import purgeCSSPlugin from "@fullhuman/postcss-purgecss";
+
+const isProduction = process.env.NODE_ENV === "production";
+
 export default {
-	plugins: {
-		tailwindcss: {},
-		autoprefixer: {},
-	},
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(isProduction && {
+      "@fullhuman/postcss-purgecss": purgeCSSPlugin({
+        content: ["./src/**/*.tsx", "./public/index.html"], // Adjust as needed
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      }),
+    }),
+  },
 };
