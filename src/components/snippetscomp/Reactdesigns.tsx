768ee3cf50402f05668fc237d4ee21af94fea3f8
Ganesh <126049941+NandhipatiGanesh@users.noreply.github.com>
2025-06-03 18:38:19 +0530

    Update Reactdesigns.tsx

--- src/components/snippetscomp/Reactdesigns.tsx
+++ src/components/snippetscomp/Reactdesigns.tsx
@@ -77,7 +77,7 @@ export default function ReactComponents() {
       style={{
         width: '100%',
         height: '100%',
-        objectFit: 'cover',
+        objectFit: 'contain',
       }}
     />
   )}
